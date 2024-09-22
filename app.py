bceb08223e699f78e30a7e5c4b7eac36c185751b
SucoJavi <mogrovejo64@gmail.com>
2024-09-22 17:09:18 -0500

    App_1

--- app.py
+++ app.py
@@ -33,3 +33,29 @@ if disp_button:  # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# crear una casilla de verificación
+build_dispersion = st.checkbox('Construir un diagrama de dispersión')
+
+if build_dispersion:  # si la casilla de verificación está seleccionada
+    st.write('Construir un diagrama de dispersión para la columna odómetro')
+
+    # crear un diagrama de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
